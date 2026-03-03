9776fce8fe3c128e41b309a804d292d706ebb9b7
abidmohdasif <abidmohdasif@gmail.com>
2026-03-03 13:38:27 +0000

    Day 2 Fixtures

--- test_health.py
+++ test_health.py
@@ -1,10 +1,18 @@
 from health import *
 
-def test_take_damage_reduces_health():
-    assert take_damage(100, 30) == 70
+def test_take_damage(player):
+    result = take_damage(player, 30)
+    assert result["health"] == 70
 
-def test_heal_adds_health():
-    assert heal(70, 30) == 100
+def test_add_damage(player):
+    result = take_damage(player, 30)
+    result = heal(player, 30)
+    assert result["health"] == 100
 
-def test_is_alive_true():
-    assert is_alive(100) == True
+def test_is_alive_true(player):
+    result = take_damage(player, 100)
+    assert is_alive(result) == False
+
+def test_add_points(new_game):
+    result = add_points(new_game, 10)
+    assert result["score"] == 10
